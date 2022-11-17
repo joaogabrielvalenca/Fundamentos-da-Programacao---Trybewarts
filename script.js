75b57b9aac92a2d0a4498dae296387f972077d36
rnogara <r.nogara.dev@gmail.com>
2022-11-17 17:33:29 -0300

    corrige lint

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const loginEmail = document.getElementById('login-email');
 const loginPassword = document.getElementById('login-password');
 const loginButton = document.getElementById('login-button');
 
-function loginChecker () {
+function loginChecker() {
+  let message = '';
   if (loginEmail.innerText === 'tryber@teste.com' && loginPassword.innerText === 123456) {
-    return alert('Olá, Tryber!');
+    message = 'Olá, Tryber!';
+  } else {
+    message = 'Email ou senha inválidos.';
   }
+  return alert(message);
 }
-
 
 loginButton.addEventListener('submit', loginChecker);
